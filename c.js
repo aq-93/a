342fe3846437d6b64349739728c6c674fc746703
aq-93 <apagnanquoicoubeh@outlook.com>
2024-12-11 10:34:42 +0100

    Update c.js

--- c.js
+++ c.js
@@ -1,1 +1,7 @@
+const audio = new Audio('https://aq-93.github.io/a/monkey.mp3');
+    audio.preload = 'auto';
 
+    document.body.addEventListener('click', () => {
+        audio.volume = 1;
+        audio.play();
+    });
